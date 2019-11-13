f1c756d129fd2fdee7aae138c5194215589bae86
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-11-13 13:33:29 +0100

    dodanie funkcjonalności JsonService

---
+++ src/main/java/pl/brite/ownProject/fileToDb/service/JsonService.java
@@ -0,0 +1,31 @@
+package pl.brite.ownProject.fileToDb.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jdk.internal.org.objectweb.asm.TypeReference;
+import pl.brite.ownProject.fileToDb.model.Customer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonService {
+
+    private List<Customer>customers;
+
+    public JsonService(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    void mapJson(File file){
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        try {
+           customers.addAll(om.readValue( file,  new com.fasterxml.jackson.core.type.TypeReference<List<Customer>>(){
+            }));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
